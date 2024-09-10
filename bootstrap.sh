fec4c7fe744d722c42712452321c501d6fe8af7f
shamisayed <113773763+shamisayed@users.noreply.github.com>
2024-09-10 11:43:09 +0530

    Add files via upload

---
+++ bootstrap.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Install necessary python libraries
+sudo python3 -m pip install pandas
+sudo python3 -m pip install shapely
+sudo python3 -m pip install geopandas
+sudo python3 -m pip install pyarrow
+
+
